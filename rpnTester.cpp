bc046c3e409bbadb606e9e7f17315464edca0369
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-11 16:08:51 -0600

    First time commit of rpnTester

---
+++ rpnTester.cpp
@@ -0,0 +1,15 @@
+//This file will contain code to start testing our stack as well as any other functions we need for the rpn calculator.
+
+#include<iostream>
+#include"stack.h"
+
+using namespace std;
+
+int main(int argc, const char *argv[]){
+  Stack *s = new Stack();
+  float x  = 1.0;
+  s->push(x);
+  
+
+
+}
