86ef5b75b5ecadfd4b81e08de75e6d2df097f8cb
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-11 16:16:20 -0600

    Made a change to test whether or not the stack was working

--- rpnTester.cpp
+++ rpnTester.cpp
@@ -9,7 +9,15 @@ int main(int argc, const char *argv[]){
   Stack *s = new Stack();
   float x  = 1.0;
   s->push(x);
-  
-
+  x = 2.0;
+  s->push(x);
+  x = 3.0;
+  s->push(x);
+  x = 4.0;
+  s->push(x);
+  cout << s->pop() << endl;
+  cout << s->pop() << endl;
+  cout << s->pop() << endl;
+  cout << s->pop() << endl;
 
 }
