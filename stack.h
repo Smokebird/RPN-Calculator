d794f936760362701909d1b249490cc835f49fe4
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-11 15:58:30 -0600

    "Declared the vector, and stack.push"

--- stack.h
+++ stack.h
@@ -2,20 +2,22 @@
 
 #ifndef STACK
 #define STACK
-
 #include<vector>
+
+using namespace std;
 
 class Stack{
  private:
-  vector<float> stack; //Note: uses floats so as to account for anything a sqrt might return
+  vector<float> *stack; //Note: uses floats so as to account for anything a sqrt might return
   
  public:
+  Stack();
   void push(float);
+
   float pop();
+
   
-
-
-}
+};
 
 
 
