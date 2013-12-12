aa576c1093cb064e6243d35725c6057afb331b8f
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-12 13:22:25 -0600

    Declared the stack and Operand Button. Callback not finished yet.

--- operandButton.h
+++ operandButton.h
@@ -3,13 +3,15 @@
 
 #include<FL/Fl_Button>
 #include"utils.h"
+#include"stack.h"
 
 class OperandButton : public Fl_Button {
  public:
- OperandButton(int X, int  Y, int W, int H, char* label)  : Fl_Button(X,Y,W,H){
+ OperandButton(int X, int  Y, int W, int H, char* label, *Stack stack)  : Fl_Button(X, Y, W, H, label){
     
   }
  private:
+  Stack *stack;
   
 
   
