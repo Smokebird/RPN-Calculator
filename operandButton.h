12a935ba0f1790f7ddef6067641b6873f9b62b0f
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-12 13:26:48 -0600

    "Committing OperandButton.h before pushing to master to see how merging works"

--- operandButton.h
+++ operandButton.h
@@ -6,16 +6,18 @@
 #include"stack.h"
 
 class OperandButton : public Fl_Button {
- public:
- OperandButton(int X, int  Y, int W, int H, char* label, *Stack stack)  : Fl_Button(X, Y, W, H, label){
-    
-  }
- private:
+private:
   Stack *stack;
+  
+public:
+ OperandButton(int X, int  Y, int W, int H, char* label, *Stack stack)  : Fl_Button(X, Y, W, H, label){
+    this->stack = stack;  
+  }
+  
   
 
   
-}
+  };
 
 
 
