97552d3995c9750e4b3277f2932fbd157954b0ef
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-12 13:43:23 -0600

    Added an integer id private variable (might not stay private for long)

--- operandButton.h
+++ operandButton.h
@@ -8,10 +8,11 @@
 class OperandButton : public Fl_Button {
 private:
   Stack *stack;
-  
+  int id;
 public:
  OperandButton(int X, int  Y, int W, int H, char* label, *Stack stack)  : Fl_Button(X, Y, W, H, label){
     this->stack = stack;  
+    this->id = ATOI(label)
   }
   
   
