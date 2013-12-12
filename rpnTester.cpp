2d9ad80ff95de8bb3da3e86bd55517240ef5d4be
Emily <eplatts14@gmail.com>
2013-12-12 14:18:09 -0600

    does not run

--- rpnTester.cpp
+++ rpnTester.cpp
@@ -2,22 +2,27 @@
 
 #include<iostream>
 #include"stack.h"
+#include<FL/Fl.H>
+#include<FL/Fl_Window.H>
+#include<FL/Fl_Box.H>
+#include<FL/Fl_Button.H>
+
+//#include"operandButton.h"
+//#include"operatorButton.h"
+
 
 using namespace std;
 
-int main(int argc, const char *argv[]){
-  Stack *s = new Stack();
-  float x  = 1.0;
-  s->push(x);
-  x = 2.0;
-  s->push(x);
-  x = 3.0;
-  s->push(x);
-  x = 4.0;
-  s->push(x);
-  cout << s->pop() << endl;
-  cout << s->pop() << endl;
-  cout << s->pop() << endl;
-  cout << s->pop() << endl;
-
+int main(int argc,  char *argv[]){
+  Fl_Window *window=new Fl_Window(100,50);
+  Fl_Box *box = new Fl_Box(20,40,260,100,"I work");
+box->box(FL_UP_BOX);
+  box->labelfont(FL_BOLD+FL_ITALIC);
+  box->labelsize(36);
+  box->labeltype(FL_SHADOW_LABEL);
+  window->end();
+  window->end();
+  window->show(argc, argv);
+ return Fl::run();
 }
+// argv can not be consint
