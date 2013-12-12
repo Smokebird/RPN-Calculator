6d627febf6ef34accc2d6e294afc630cec4f6fcb
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-12 13:11:48 -0600

    " First time commit of operand button header. "

---
+++ operandButton.h
@@ -0,0 +1,20 @@
+#ifndef OPERANDBUTTON
+#define OPERANDBUTTON
+
+#include<FL/Fl_Button>
+#include"utils.h"
+
+class OperandButton : public Fl_Button {
+ public:
+ OperandButton(int X, int  Y, int W, int H, char* label)  : Fl_Button(X,Y,W,H){
+    
+  }
+ private:
+  
+
+  
+}
+
+
+
+#endif
