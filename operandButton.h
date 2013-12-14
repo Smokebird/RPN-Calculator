d63401ad8b5576f2a82f7a2196a3d19e71458335
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-14 16:18:27 -0600

    Having trouble getting the callback to work right. It works right now, but not really in the way I had intended. Static variables are obnoxious.

--- operandButton.h
+++ operandButton.h
@@ -1,22 +1,30 @@
 #ifndef OPERANDBUTTON
 #define OPERANDBUTTON
 
-#include<FL/Fl_Button>
+#include<FL/FL_Button.H>
 #include"utils.h"
 #include"stack.h"
+#include<cstdlib>
+
+using namespace std;
 
 class OperandButton : public Fl_Button {
 private:
+  
+public:
   Stack *stack;
   float id;
-public:
- OperandButton(int X, int  Y, int W, int H, char* label, *Stack stack)  : Fl_Button(X, Y, W, H, label){
+
+  OperandButton(int X, int  Y, int W, int H, char* label, Stack* stack)  : Fl_Button(X, Y, W, H, label){
     this->stack = stack;  
-    int temp = ATOI(label);
+    int temp = std::atoi(label);
     this->id = temp;
   }
-  static void opCB(Fl_Widget, void);
-  
+  static void opCB(Fl_Widget* w, void*){
+    OperandButton *myButton = (OperandButton *)w;
+    myButton->stack->push(myButton->id);
+  }
+  static char num[20];
 
   
   };
