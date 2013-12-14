6cb05885fc80d780c3c337c50d8df4a0e62a6073
Emily <eplatts14@gmail.com>
2013-12-14 16:33:18 -0600

    ?

--- operandButton.h
+++ operandButton.h
@@ -1,7 +1,7 @@
 #ifndef OPERANDBUTTON
 #define OPERANDBUTTON
 
-#include<FL/Fl_Button>
+#include<FL/Fl_Button.H>
 #include"utils.h"
 #include"stack.h"
 
@@ -10,12 +10,12 @@ private:
   Stack *stack;
   float id;
 public:
- OperandButton(int X, int  Y, int W, int H, char* label, *Stack stack)  : Fl_Button(X, Y, W, H, label){
+ OperandButton(int X, int  Y, int W, int H, char* label, Stack *stack)  : Fl_Button(X, Y, W, H, label){
     this->stack = stack;  
     int temp = ATOI(label);
     this->id = temp;
   }
-  static void opCB(Fl_Widget, void);
+  // static void opCB(Fl_Widget, void); got errors that i do not undertand
   
 
   
