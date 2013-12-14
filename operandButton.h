30e5678684a3f31b175b13ec2fc314d79e1cf36e
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-14 17:06:17 -0600

    Commiting before a pull

--- operandButton.h
+++ operandButton.h
@@ -5,6 +5,7 @@
 #include"utils.h"
 #include"stack.h"
 #include<cstdlib>
+#include<vector>
 
 using namespace std;
 
@@ -14,17 +15,20 @@ private:
 public:
   Stack *stack;
   float id;
+  
 
   OperandButton(int X, int  Y, int W, int H, char* label, Stack* stack)  : Fl_Button(X, Y, W, H, label){
     this->stack = stack;  
     int temp = std::atoi(label);
     this->id = temp;
+    
   }
   static void opCB(Fl_Widget* w, void*){
     OperandButton *myButton = (OperandButton *)w;
-    myButton->stack->push(myButton->id);
+    OperandButton::num.push_back(myButton->id);  
+    //myButton->stack->push(myButton->id);
   }
-  static char num[20];
+  static vector<float> num;
 
   
   };
