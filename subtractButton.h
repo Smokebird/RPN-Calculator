f8787dfa258795fe7e7fe3fb89fd53c8bf9f7f27
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-15 15:56:34 -0600

    Added in the actual subtraction functionality of the subtraction button. This can be a decent model for the other buttons I think (especially the binary ones)

--- subtractButton.h
+++ subtractButton.h
@@ -2,30 +2,42 @@
 #define SUBTRACTBUTTON
 
 #include<FL/Fl_Button.H>
-#include"utils.h"
+//#include"utils.h"
 #include"stack.h"
 #include<cstdlib>
-
+#include<string>
+#include<sstream>
 using namespace std;
 
 class SubtractButton : public Fl_Button {
  public:
-
-  Stack *stack;
-  float id;
-  float op1;
-  float op2;
-
- SubtractButton(int X, int Y, int W, int H, char* label, Stack *stack) : Fl_Button(X,Y,W,H,label){
-    this->stack =stack;
-
+    float from_string( const std::string &s) {
+    stringstream ss(s);
+    float result;
+    ss >> result;    // TODO handle errors
+    return result;
   }
 
-
-void sbCB(Fl_Widget * w, void*){
-  SubtractButton *myButton = (SubtractButton *)w;
-  myButton->stack->push(myButton->id);
-}
+  string *myStr;
+  Stack *stack;
+  //float id;
+  
+  
+ SubtractButton(int X, int Y, int W, int H, char* label, Stack *stack, string* str) : Fl_Button(X,Y,W,H,label){
+    this->stack = stack;
+    this->myStr = str;
+  }
+  
+  
+  static void sbCB(Fl_Widget * w, void*){
+    float op1;
+    float op2;
+    SubtractButton *myButton = (SubtractButton *)w;
+    op2 =  myButton->from_string(*myButton->myStr);
+    op1 = myButton->stack->pop();
+    myButton->stack->push(op1-op2);
+    cout <<"The number you pushed on the stack was : " << op1-op2 << endl;
+  }
 };
 
 #endif
