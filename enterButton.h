aa6f7b475a8afaba61ede9cbec47df6047ff0631
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-15 15:11:38 -0600

    Current the enter Button is pushing numbas onto the stackgit add enterButton.h

---
+++ enterButton.h
@@ -0,0 +1,61 @@
+#ifndef ENTERBUTTON
+#define ENTERBUTTON
+
+#include<FL/Fl_Button.H>
+//#include"utils.h"
+#include"stack.h"
+#include<cstdlib>
+#include<vector>
+#include"operandButton.h"
+#include<sstream>
+using namespace std;
+
+
+
+
+class EnterButton : public Fl_Button {
+private:
+  
+public:
+  float from_string( const std::string &s) {
+    stringstream ss(s);
+    float result;
+    ss >> result;    // TODO handle errors
+    return result;
+  }
+
+  
+  Stack *stack;
+  
+  string *myStr;
+
+ EnterButton(int X, int  Y, int W, int H, char* label, Stack* stack, string* numLst )  : Fl_Button(X, Y, W, H, label){
+    this->stack = stack;
+    this->myStr = numLst;
+    this->copy_label(label);
+    
+  }
+  
+  
+  
+  static void enterCB(Fl_Widget* w, void*){
+    
+    EnterButton *myButton = (EnterButton *)w;
+    float number = 0;
+    number = myButton->from_string(*myButton->myStr);
+    cout <<"The number you pushed on the stack was : " << number << endl;
+    myButton->stack->push(number);    
+    
+    (*myButton->myStr) = "";
+    
+
+   
+    }
+  
+
+  
+  };
+
+
+
+#endif
