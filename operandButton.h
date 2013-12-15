44de111504144863d37def6d4cbe5590adc1e821
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2013-12-15 15:12:45 -0600

    works well with the enter button right now. Compiles and does it's job

--- operandButton.h
+++ operandButton.h
@@ -6,32 +6,38 @@
 #include"stack.h"
 #include<cstdlib>
 #include<vector>
+#include<string>
+#include<iostream>
 
 using namespace std;
 
 class OperandButton : public Fl_Button {
+
 private:
   
 public:
   
   Stack *stack;
-  float id;
-  
+  string id;
+  string *myStr;
 
-  OperandButton(int X, int  Y, int W, int H, char* label, Stack* stack)  : Fl_Button(X, Y, W, H, label){
-    this->stack = stack;  
-    int temp = std::atoi(label);
-    this->id = temp;
+ OperandButton(int X, int  Y, int W, int H, char* label, Stack* stack, string *numLst)  : Fl_Button(X, Y, W, H, label){
+    this->stack = stack;
+    this->myStr = numLst;
+    //int temp = std::atoi(label);
+    this->id = label;
     
   }
   
   
   
   static void opCB(Fl_Widget* w, void*){
-    static vector<float> num;
     OperandButton *myButton = (OperandButton *)w;
-    num.push_back(myButton->id);  
+    string temp = (*myButton->myStr) + myButton->id;
+    (*myButton->myStr)= temp;
     //myButton->stack->push(myButton->id);
+
+
   }
   
 
