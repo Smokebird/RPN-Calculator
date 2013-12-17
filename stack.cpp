77ab0acb4df6a6183366dcd44c1a2efc84caac2c
Emily <eplatts14@gmail.com>
2013-12-16 22:22:28 -0600

    >

--- stack.cpp
+++ stack.cpp
@@ -9,9 +9,11 @@ Stack::Stack(){
 } 
 
 void Stack::push(float x){
+  cout << "push" << endl;
   stack->push_back(x);//insert(stack->begin(),x);
-  
+  cout << "push" << endl;
   for(int i = 0; i< this->stack->size(); i++){
+    
     std::cout<<(*stack)[i];
   }
   cout << endl;
